61310cd3ca17efb73d7f2cc019135e978b3909a4
aguimont5 <aidanguimont5@gmail.com>
2024-01-18 21:44:55 -0500

    Created simulation loop

--- simulate.py
+++ simulate.py
@@ -1,6 +1,14 @@
 #load libraries
 import pybullet as p
+import time as t
 
 #create physics client
 physicsClient = p.connect(p.GUI)
+
+#simulation loop
+for i in range(0,1000):
+    p.stepSimulation()
+    t.sleep(1/60)
+    print(i)
+
 p.disconnect()
